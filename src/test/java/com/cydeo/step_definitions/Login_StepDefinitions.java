aad47bbceea1e9992250c068d63df474cd3c2559
gabs1311 <gabs.antonovici@gmail.com>
2022-07-06 15:36:03 +0300

    Login.feature

--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -25,4 +25,17 @@ public class Login_StepDefinitions {
       //  throw new io.cucumber.java.PendingException();
         System.out.println("User should see the dashboard");
     }
+
+
+    @When("user enters student username")
+    public void user_enters_student_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters student password")
+    public void user_enters_student_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
