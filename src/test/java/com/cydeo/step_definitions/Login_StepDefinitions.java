e4ef7a428066620ce32298949d99608cc8c3ee86
gabs1311 <gabs.antonovici@gmail.com>
2022-07-06 16:09:11 +0300

    Login.feature

--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -1,5 +1,6 @@
 package com.cydeo.step_definitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -25,6 +26,17 @@ public class Login_StepDefinitions {
       //  throw new io.cucumber.java.PendingException();
         System.out.println("User should see the dashboard");
     }
+/*  //i can create the manually, like the ones underneath
+
+    @When("user enters student username")
+    public void userEntersStudentUsername() {
+    }
+
+    @And("user enters student password")
+    public void userEntersStudentPassword() {
+    }
+
+ */
 
 
     @When("user enters student username")
@@ -37,5 +49,22 @@ public class Login_StepDefinitions {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
+
+
+    @When("user enters admin username")
+    public void user_enters_admin_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters admin password")
+    public void user_enters_admin_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
 
 }
