b9500f9c089d59f976803cfaead9e3fc10a4bb07
gabs1311 <gabs.antonovici@gmail.com>
2022-07-07 12:29:56 +0300

    day15 Hooks.java

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,6 +1,7 @@
 package com.cydeo.step_definitions;
 
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 /*
@@ -14,8 +15,12 @@ public class Hooks {
     public void setupScenario(){
         System.out.println("===Setting up browser using cucumber  @Before");
 
+    }
 
-
+    @After
+    public void teardownScenario(){
+        System.out.println("===Closing browser using cucumber @After");
+        System.out.println("===Scenario ended/ Take screenshot if failed");
     }
 
 }
