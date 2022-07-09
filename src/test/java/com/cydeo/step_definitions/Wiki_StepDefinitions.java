4f14dda5287195a252633fb03d83fa53542358c0
gabs1311 <gabs.antonovici@gmail.com>
2022-07-09 10:06:58 +0300

    day16 wikiSearchPage.j

--- src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
@@ -38,5 +38,8 @@ public class Wiki_StepDefinitions {
 
     @Then("User sees {string} is in the main header")
     public void userSeesIsInTheMainHeader(String arg0) {
+
+        Assert.assertTrue(wikiSearchPage.mainHeader.isDisplayed());
+        Assert.assertTrue(wikiSearchPage.mainHeader.getText().equals("string"));
     }
 }
